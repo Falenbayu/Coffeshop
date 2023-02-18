3242e3ea1811261dd4e67603cc55100a9a5c633f
Falen Bayu A <86699460+Falenbayu@users.noreply.github.com>
2023-02-18 23:37:47 +0700

    navbar

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,12 @@
 const navbarNav = document.querySelector('.navbar-nav');
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active')
-}
+};
+
+//klik diluar sidebar
+const humburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function(e){
+    if(!humburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+});
