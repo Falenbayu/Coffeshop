6222ec7b56f184727fccf10846f9f9efdbf97ebf
Falen Bayu A <86699460+Falenbayu@users.noreply.github.com>
2023-02-18 23:02:56 +0700

    js

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+//toggle class active
+const navbarNav = document.querySelector
+('.navbar-nav');
+//ketika menu di klik
+document.querySelector('#humburger-menu').
+onClick = () => {
+     navbarNav.classList.toggle('active');
+};
