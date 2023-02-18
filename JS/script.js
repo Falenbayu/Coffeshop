7a25b53f3ee707b7fa99adc492457da3a41bbee4
Falen Bayu A <86699460+Falenbayu@users.noreply.github.com>
2023-02-18 23:27:43 +0700

    navbar rev 3

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,5 @@
 //toggle class active
 const navbarNav = document.querySelector('.navbar-nav');
-
-//ketika menu di klik
-document.querySelector('#hamburger-menu').onClick = () => {
+document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active')
 }
